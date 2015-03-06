f27b602c7ba45c104d4ab71b2a38d27f00156a19
Robjen12 <Robertjensen1985@hotmail.com>
2015-03-06 12:39:41 +0100

    Started coding

---
+++ inc/HTMLTemplate.php
@@ -0,0 +1,40 @@
+<?php
+
+
+
+$header = <<<END
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>GamersKeep - Where gamers meet </title>
+		<link rel="stylesheet" href=css/style.css>
+	</head>
+
+	<body>
+		<div id="header">
+			<div id="container">
+				<div class="row">
+					<div class="col-md-12"></div>
+				</div>
+			</div>
+		</div>
+END;
+$footer = <<<END
+
+	<div id="footer">
+		<div id="container">
+			<div class="row">
+				<div class="col-md-12"></div>
+			</div>
+		</div>
+
+	</div>
+
+	</body>
+
+
+</html>
+
+END;
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include_once("inc/HTMLTemplate.php");
+
+$content = <<<END
+
+END;
+
+echo $header;
+echo $content;
+echo $footer;
+?>
