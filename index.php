bb8b47a92bfeef11930b91d2d180f94ec9cde0dc
Robjen12 <Robertjensen1985@hotmail.com>
2015-03-09 12:29:38 +0100

    testing

--- index.php
+++ index.php
@@ -3,19 +3,33 @@ include_once("inc/HTMLTemplate.php");
 
 $content = <<<END
 	
-	<div id="mitt">
+	<div id="news">
 		<div id="container">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="toplistname"></div>
+				</div>
+
+				<div class="col-md-4">
+					<div class="latestlistname">
+
+					</div>
+				</div>
+
+				<div class="col-md-3">
+					<div class="ads">
+
+					</div>
+				</div>
+			</div>
 			<div class="row">
 				<div class="col-md-4">
 					<div class="toplist"></div>
 				</div>
-
 				<div class="col-md-4">
-					<div class="latestlist">
-
-					</div>
+					<div class="latestlist"></div>
 				</div>
-				<div class="col-md-4"></div>
+				<div class="col-md-3"></div>
 			</div>
 		</div>
 	</div>
