8dfac42030caa55ca68ee9c3b3f037b7bfd337ed
Robjen12 <Robertjensen1985@hotmail.com>
2015-03-23 13:19:53 +0100

    Flyttade inloggning till höger

--- login.php
+++ login.php
@@ -10,19 +10,26 @@ $content = <<<END
 	<title>GamersKeep - Where gamers meet</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 </head>
 
 <body>
 	<div id="container">
-		<div class="login">
-		<h1>Enter the keep</h1>
-			<form action="login.php" method="post" id="login-form">
-				<input type="text" id="keepername" name="keepername" value="" placeholder="Skriv in användarnamn"></br></br>
-				<input type="" id="pw" name="pw" value="" placeholder="Skriv in lösenord"></br></br>
-				<button type="submit" id="submit" value="Logga in">Logga in</button>
-				<button type="submit" id="submit" value="Registrera sig">Registrera sig</button>
-			</form>
-		</div>     <!-- login -->
+		<div class="row">
+			<div class="col-md-6"></div>
+			<div class="col-md-2"></div>
+			<div class="col-md-4 center-block">
+				<div class="login">
+					<h1>Enter the keep</h1>
+					<form action="login.php" method="post" id="login-form">
+						<input type="text" id="keepername" name="keepername" value="" placeholder="Skriv in användarnamn"></br></br>
+						<input type="" id="pw" name="pw" value="" placeholder="Skriv in lösenord"></br></br>
+						<button type="submit" id="submit" value="Logga in">Logga in</button>
+						<button type="submit" id="submit" value="Registrera sig">Registrera sig</button>
+					</form>
+				</div>     <!-- login -->
+			</div>   <!-- col-md-4 -->
+		</div>
 	</div>
 </body>
 
