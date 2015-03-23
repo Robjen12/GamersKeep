db8fdbfac92b2dd55616f1f64b2fd1a12fc17521
Robjen12 <Robertjensen1985@hotmail.com>
2015-03-23 12:43:33 +0100

    login class tillagd

--- login.php
+++ login.php
@@ -9,18 +9,20 @@ $content = <<<END
 <head>
 	<title>GamersKeep - Where gamers meet</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-	<div="container">
-	<h1>Enter the keep</h1>
-		<form action="login.php" method="post" id="login-form">
-			<input type="text" id="keepername" name="keepername" value="" placeholder="Skriv in användarnamn"></br></br>
-			<input type="" id="pw" name="pw" value="" placeholder="Skriv in lösenord"></br></br>
-			<button type="submit" id="submit" value="Logga in">Logga in</button>
-			<button type="submit" id="submit" value="Registrera sig">Registrera sig</button>
-		</form>
+	<div id="container">
+		<div class="login">
+		<h1>Enter the keep</h1>
+			<form action="login.php" method="post" id="login-form">
+				<input type="text" id="keepername" name="keepername" value="" placeholder="Skriv in användarnamn"></br></br>
+				<input type="" id="pw" name="pw" value="" placeholder="Skriv in lösenord"></br></br>
+				<button type="submit" id="submit" value="Logga in">Logga in</button>
+				<button type="submit" id="submit" value="Registrera sig">Registrera sig</button>
+			</form>
+		</div>     <!-- login -->
 	</div>
 </body>
 
