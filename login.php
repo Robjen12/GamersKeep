9aaea9e2d8090a0e2ec4948672a9ddc51aede144
Robjen12 <Robertjensen1985@hotmail.com>
2015-03-23 11:34:49 +0100

    fixat formular

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+
+
+$header = <<<END
+
+
+<html>
+
+<head>
+
+</head>
+
+<body>
+
+	<form action="login.php" method="post" id="login-form">
+	<label for="keepername">Användarnamn</label>
+	<input type="text" id="keepername" name="keepername" value="" placeholder="Skriv in användarnamn"></br>
+	<label for="pw">Lösenord</label>
+	<input type="" id="pw" name="pw" value="" placeholder="Skriv in lösenord"></br>
+	<button type="submit" id="submit" value="Logga in">Enter the keep</button>
+	</form>
+</body>
+
+
+
+
+</html>
+END;
+?>
