aaf949c6d8e62cabb655d58a8b877b5db2e7b780
Robjen12 <Robertjensen1985@hotmail.com>
2015-03-24 11:08:17 +0100

    Registrering ongoing

--- register.php
+++ register.php
@@ -17,17 +17,25 @@ $content = <<<END
 <body>
 	<div id="container">
 		<div class="row">
-			<div class="col-md-2"></div>
+			<div class="col-md-4"></div>
 			<div class="col-md-8">
 				<div class="register">
+					<h1>Registrering</h1>
 					<form action="register.php" method="post" id="register-form">
-						<label for="keepername">Användarnamn:</label>
-						<input type="text" id="keepername" name="keepername" value="">
-						<label for="Lösenord">
+						<label for="keepername">Användarnamn:</label></br>
+						<input type="text" id="keepername" name="keepername" value=""></br>
+						<label for="fname">Förnamn:</label></br>
+						<input type="text" id="fname" name="fname" value""></br>
+						<label for="lname">Efternamn:</label></br>
+						<input type="lname" id="lname" name="lname" value=""></br>
+						<label for="email">E-postadress:</label></br>
+						<input type="text" id="email" name="email" value=""></br>
+						<label for="pw">Lösenord:</label></br>
+						<input type="password" id="pw" name="pw" value""></br></br>
+						<button type="submit" value="submit" value="Skapa konto">Skapa Konto</button>
 					</form>
 				</div>
 			</div>
-			<div class="col-md-2"></div>
 		</div>
 	</div>
 </body>
