a5c512f178ba5f7f929ccca534bb5a0fef29ad44
Robjen12 <Robertjensen1985@hotmail.com>
2015-03-27 14:19:02 +0100

    Nu kan man lägga till både rec och gui

--- guide_review.php
+++ guide_review.php
@@ -4,36 +4,58 @@ include_once("inc/Connstring.php");
 include_once("inc/HTMLTemplate.php");
 
 $table = "guidereviewinfo";
-$keepername = "keepername";
+$keepername = $_SESSION['keepername'];
 
-if(isset($_POST['publish_guide_review']))
-{
 
 	if(!empty($_POST))
 	{
 
-		$guidetitle = $_POST['title'];
-		$guidetext = $_POST['nicEdit'];
+		$title	= $_POST['title'];
+		$text 	= $_POST['nicEdit'];
+		$grade	= $_POST['grade'];
 
-		$guidetitle = utf8_encode($mysqli->real_escape_string($guidetitle));
-		$guidetext  = utf8_encode($mysqli->real_escape_string($guidetext));
+		$title = utf8_encode($mysqli->real_escape_string($title));
+		$text  = utf8_encode($mysqli->real_escape_string($text));
 
+		if(isset($_POST['guide']))
+		{
 		$query = <<<END
 
 			INSERT INTO {$table}(title, text, timestamp)
-			VALUES ('{$guidetitle}', '{$guidetext}', CURRENT_TIMESTAMP);
+			VALUES ('{$title}', '{$text}', CURRENT_TIMESTAMP);
 END;
 		 $res = $mysqli->query($query) or die("Could not query database" . $mysqli->errno . 
         " : " . $mysqli->error);
+
 		 $query = <<<END
 
 		 	INSERT INTO userguidereview(grid, keepername)
-		 	VALUES (LAST_INSERT_ID(), {$keepername})
+		 	VALUES (LAST_INSERT_ID(), '{$keepername}');
 END;
 		 $res = $mysqli->query($query) or die("Could not query database" . $mysqli->errno . 
         " : " . $mysqli->error);
+		}
+		else if(isset($_POST['review']))
+		{
+		$query = <<<END
+
+			INSERT INTO {$table}(title, text, timestamp, grade)
+			VALUES ('{$title}', '{$text}', CURRENT_TIMESTAMP, '{$grade}' );
+END;
+		$res = $mysqli->query($query) or die("Could not query database" . $mysqli->errno . 
+        " : " . $mysqli->error);
+
+        $query = <<<END
+
+        	INSERT INTO userguidereview(grid, keepername)
+        	VALUES (LAST_INSERT_ID(), '{$keepername}');
+END;
+		$res = $mysqli->query($query) or die("Could not query database" . $mysqli->errno . 
+        " : " . $mysqli->error);
+		}
 	}
-}
+
+
 $content = <<<END
 
 <!DOCTYPE html>
@@ -45,14 +67,15 @@ $content = <<<END
 			<div class="row">
 				<div class="col-md-4">
 					<div id="guide_review">
-						
-						<form action="guide_review.php" method="post" id="">
-							<input type="checkbox" name="guide" value="Guide">Guide
-							<input type="checkbox" name="recension" value="Recension">Recension</Br></br>
+						<form action="guide_review.php" method="post" id="guide_review_form">
 							<label for="title">Titel</label></Br>
 							<input type="text" id="title" name="title" value="" placeholder="Ange titeln"><br></br>
+							<input type="checkbox" id="guidecheck" name="guide" value="Guide">Guide
+							<input type="checkbox" id="reviewcheck" name="review" value="Review">Recension</Br></br>
 							<label for="information">Innehållet:</label>
 							<textarea id="nicEdit" name="nicEdit" cols="80" rows="20"></textarea></br>
+							<label for="grade">Betyg</label><br>
+							<input type="text" id="grade" name="grade" value="" placeholder="Ange betyg här">
 							<button type="submit" id="publish" name="publish_guide_review" value="Publicera">Publicera innehållet</button>
 
 						</form>
@@ -62,11 +85,22 @@ $content = <<<END
 				<div class="col-md-4"></div>
 			</div>
 		</div>
+		
 		<script type="text/javascript" src="js/nicEdit.js"></script>
 		<script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>
-		
+
+	<script>
+
+	$(document).ready(function(){
+		$('#grade').hide();
+		$('#reviewcheck').click(function(){
+			$('#grade').show();
+		});
+	});	
+	</script>
 	</body>
 </html>
+
 END;
 
 echo $header;
