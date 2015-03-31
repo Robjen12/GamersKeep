845d8eaf765897f24d19cbeaf1a0a176fd2eabc9
Robjen12 <Robertjensen1985@hotmail.com>
2015-03-31 13:56:59 +0200

    nu funkar radion

--- guide_review.php
+++ guide_review.php
@@ -126,6 +126,20 @@ $content = <<<END
 	});	
 	</script>
 
+	<script>
+	$(document).ready(function () {
+  $('input[type=radio]').prop('checked', false);
+  $('input[type=radio]:first').prop('checked', true)
+
+  $('input[type=radio]').click(function (event) {
+    $('input[type=radio]').prop('checked', false);
+    $(this).prop('checked', true);
+
+  
+  });
+});
+	</script>
+
 
 END;
 
