b87b1bbb2b240f15a36c92282edcd74a292a0483
Robjen12 <Robertjensen1985@hotmail.com>
2015-04-07 12:03:59 +0200

    no margin

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ else
 }*/
 $content = <<<END
 				
-			<div id="container">
+			<div class="container">
 				<div class="row margin-top-50">
 			
 					<div class="col-md-4 col-sm-4 panel panel-default">
@@ -143,7 +143,7 @@ $content = <<<END
 
 
 
-				<div class="row margin-left-60">
+				<div class="row">
 					<div class="col-md-4 col-sm-4 panel panel-default">
 
 		  					<div class="panel-heading">Topplista recensionerna</div>
