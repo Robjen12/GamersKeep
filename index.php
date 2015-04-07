14a178a565b9a32361a0266a21ea88dd0eb27112
Robjen12 <Robertjensen1985@hotmail.com>
2015-04-07 15:41:06 +0200

    fungerar fint

--- index.php
+++ index.php
@@ -107,7 +107,10 @@ $content = <<<END
 
 		  					<div class="panel-body">
 
-			  					
+			  					<p>Toplist innehall.</p> <p>But I must explain to you how all this mistaken idea of denouncing pleasure
+				  					and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of
+				  					the great explorer of the truth, the master-builder of human happiness.</p>
+				  					<p> Master-builder without using THE KRAGGLE!</p>
 
 		  					</div><!-- panel body -->
 
