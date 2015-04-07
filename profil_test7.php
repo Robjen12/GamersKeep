df294cbc90a69fc6705bdc000e9008da3d79a9ea
DorteTA <dorteta@gmail.com>
2015-04-07 19:32:41 +0200

    profil naestan klar whee

--- profil_test7.php
+++ profil_test7.php
@@ -1,10 +1,15 @@
 <?php
 include_once("inc/HTMLTemplate.php");
 
+
+
+
 $content = <<<END
+
+		<link href='css/style_profil.css' rel='stylesheet' type='text/css'>
 				
 			<div class="container">
-				<div class="row">
+				<div class="row margin-top-100">
 			
 					<div class="col-md-3 col-sm-3 pull-left">
 
@@ -25,70 +30,89 @@ $content = <<<END
 	  						</div>
 
 	  						<div class="column-left-bottom">
-	  							<p>Om mig</p>
+	  							<p>Sista nytt</p>
 
 	  							<p>Sociala Medier</p>
 	  							
-	  							<p>Vanner</p>
-	  							
+	  							<p>Vanner</p>	  							
+
 	  							</div><!-- col md 3>
 
 	  							<p>Vänner</p>		
 
 	  						</div><!-- column left bottom -->
 						
-						</div><!-- row profil -->
+						</div><!-- row -->
 
-					</div><!-- col md 3 torn -->
-						
-						
-					
-
-					<div class="col-md-9 col-sm-9 profil column-top-right pull-left">
-
-						<div class="col-md-9 profil-right info panel-default">
-
-						col md 6
 						</div>
-
-						<div class="col-md-3 pull-left reklam pull-right">
-
-						col md 3
-						</div>
-					</div>
-	  					
-
 						
+						<div class="col-md-6 col-sm-6 panel-width-550px panel panel-default">
 
-					
-	  					<div class="panel-heading">Sociala Medier</div>
+	  					<div class="panel-heading width-500px">Din Profil</div>
 
 		  					<div class="panel-body">
 
-		  						<p>Här kan du skriva mera om dig själv, ex vilka spel du gillar och liknande.</p>
-
-		  						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris a augue eleifend,
-		  							tempor felis eu, rutrum nisi. Curabitur at nisl urna. Aliquam erat volutpat.
-		  						  	Phasellus congue vehicula tristique. Maecenas vulputate ultricies laoreet.
-		  						   	Suspendisse potenti. Praesent nec tellus in nulla commodo consectetur.
-		  						   	Integer mauris eros, pellentesque sed blandit et, aliquet eget magna. Quisque
-		  						   	vel dui justo.</p>
-
-		  						<p> </p>
-
+			  					<p>Namn</p> <p>But I must explain to you how all this mistaken idea of denouncing pleasure
+			  					and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of
+			  					the great explorer of the truth, the master-builder of human happiness.</p>
+			  					<p> Master-builder without using THE KRAGGLE!</p>
+			  						  			
 		  					</div><!-- panel body -->
-						
+
 						</div><!-- panel heading -->
 
-
-
-					<div class="col-md-3 col-sm-3 ads pull-right">
+						<div class="col-md-3 col-sm-3 ads">
 
 					<!-- Reklam karusel -->
 					
 	  					<img src="http://placehold.it/200x350">
 	  					
 					</div><!-- reklam kolumn -->
+					
+
+					<div class="col-md-3 col-sm-3 panel-width-330px panel panel-default pull-left">
+
+	  					<div class="panel-heading panel-heading-340px">Datorspecifikationer</div>
+
+		  					<div class="panel-body">
+ 
+		  						<p>Senaste listorna innehall: No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but
+		  						because those who do not know
+		  						how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who
+		  						loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances
+		  						occur in which toil and pain can procure him some great pleasure.</p>
+
+		  						<p>Senaste listorna innehall: No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but
+		  						because those who do not know
+		  						how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who
+		  						loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances
+		  						occur in which toil and pain can procure him some great pleasure.</p>
+
+		  						<p>Senaste listorna innehall: No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but
+		  						because those who do not know
+		  						how to pursue pleasure rationally encounter consequences that are extremely painful. </p>
+
+		  					</div>
+						
+						</div><!-- panel heading -->
+
+						<div class="col-md-3 col-sm-3 panel-width-190px panel panel-default pull-left">
+
+	  					<div class="panel-heading panel-heading-200px">Länkar</div>
+
+		  					<div class="panel-body">
+ 
+		  						<p>Senaste listorna innehall: No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but
+		  						because those who do not know
+		  						how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who
+		  						loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances
+		  						occur in which toil and pain can procure him some great pleasure.</p>
+
+		  					</div>
+						
+						</div><!-- panel heading -->
+
+
 
 					
 					</div><!-- kolumn 2 -->
