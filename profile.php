1f1e1c5a5609ceaae0df11d3a3a4a94d6edac864
Robjen12 <Robertjensen1985@hotmail.com>
2015-04-08 08:42:16 +0200

    va

--- profile.php
+++ profile.php
@@ -2,7 +2,12 @@
 include_once("inc/HTMLTemplate.php");
 
 
+
+
 $content = <<<END
+
+		<link href='css/style_profil.css' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="css/style.css">
 				
 			<div class="container">
 				<div class="row margin-top-100">
@@ -10,33 +15,68 @@ $content = <<<END
 					<div class="col-md-3 col-sm-3 pull-left">
 
 	  					<div class="row profil">
+	  					
 	  						<div class="column-left-top">
-	  							<p>sat in overste del av torn i bakgrund har med en class i column left top</p>
+	  					
+	  							<p>Välkommen</p>
+  	  					
   	  						</div>	  					
-	  					</div><!-- row profil left top -->
+	  					
+	  						<div class="column-left-center text-center">	  							
+	  					
+	  								<img src="images/profil_bild.png">	  							
 
-	  					<div class="row profil">  							
-	  						<div class="column-left-center">
-	  							<p>left center</p>
+	  							<p><b>Keepername</b></p>
+	  					
 	  						</div>
-	  					</div><!-- row profil left top -->
 
-	  					<div class="row profil">
 	  						<div class="column-left-bottom">
-	  							<p>left bottom</p>
-	  						</div>
-						</div><!-- row -->
-					</div>
+	  							<p>Sista nytt</p>
 
+	  							<p>Sociala Medier</p>
+	  							
+	  							<p>Vanner</p>	  							
+
+	  							</div><!-- col md 3>
+
+	  							<p>Vänner</p>		
+
+	  						</div><!-- column left bottom -->
 						
-					
+						</div><!-- row -->
 
-					<div class="col-md-6 col-sm-6 panel panel-default pull-left">
+						</div>
+						
+						<div class="col-md-6 col-sm-6 panel-width-550px panel panel-default pull-left">
 
-	  					<div class="panel-heading">Senaste listorna</div>
+	  					<div class="panel-heading panel-heading-560px">Din Profil <img src="images/pen.png" width="30px" class="pull-right"></div>
 
 		  					<div class="panel-body">
 
+			  					<p>Namn</p> <p>But I must explain to you how all this mistaken idea of denouncing pleasure
+			  					and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of
+			  					the great explorer of the truth, the master-builder of human happiness.</p>
+			  					<p> Master-builder without using THE KRAGGLE!</p>
+			  						  			
+		  					</div><!-- panel body -->
+
+						</div><!-- panel heading -->
+
+						<div class="col-md-3 col-sm-3 ads">
+
+					<!-- Reklam karusel -->
+					
+	  					<img src="http://placehold.it/200x350">
+	  					
+					</div><!-- reklam kolumn -->
+					
+
+					<div class="col-md-3 col-sm-3 panel-width-330px panel panel-default pull-left">
+
+	  					<div class="panel-heading panel-heading-340px">Datorspecifikationer</div>
+
+		  					<div class="panel-body">
+ 
 		  						<p>Senaste listorna innehall: No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but
 		  						because those who do not know
 		  						how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who
@@ -57,15 +97,25 @@ $content = <<<END
 						
 						</div><!-- panel heading -->
 
+						<div class="col-md-3 col-sm-3 panel-width-190px panel panel-default pull-left">
+
+	  					<div class="panel-heading panel-heading-200px">Länkar</div>
+
+		  					<div class="panel-body">
+ 
+		  						<p>Senaste listorna innehall: No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but
+		  						because those who do not know
+		  						how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who
+		  						loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances
+		  						occur in which toil and pain can procure him some great pleasure.</p>
+
+		  					</div>
+						
+						</div><!-- panel heading -->
 
 
-					<div class="col-md-3 col-sm-3 ads">
 
-					<!-- Reklam karusel -->
 					
-	  					<img src="http://placehold.it/200x350">
-	  					
-					</div><!-- reklam kolumn -->
 					</div><!-- kolumn 2 -->
 					</div><!-- kolumn 2 -->
 
@@ -82,5 +132,3 @@ echo $header;
 echo $content;
 echo $footer;
 ?>
-placehold.it
-placehold.it
