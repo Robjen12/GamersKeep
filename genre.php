e8ae5704695890d4801f4cc49f71937c21aabe86
Robjen12 <Robertjensen1985@hotmail.com>
2015-04-17 12:11:25 +0200

    it wörks

--- genre.php
+++ genre.php
@@ -6,6 +6,10 @@ include_once("inc/Connstring.php");
 $keeper = $_SESSION['keeperid'];
 $grid = isset($_GET['grid']) ? $_GET['grid'] : '';
 $showgrade = "";
+$text = "";
+$title = "";
+$timestamp = "";
+$comments = "";
 
 	$query = <<<END
 
@@ -23,19 +27,63 @@ $query = <<<END
 END;
 $res = $mysqli->query($query);
 
-$row = $res->fetch_object();
 
-$title = utf8_decode(htmlspecialchars($row->title));
-$text  = utf8_decode(htmlspecialchars($row->text));
-$timestamp = strtotime($row->timestamp);
-$timestamp = date("d M Y H:i", $timestamp);
-$grade = $row->grade;
+if($res->num_rows ==1)
+{
+	$row = $res->fetch_object();
 
-if($grade > 0){
-	$showgrade = <<<END
-	Betyg: {$grade}
+	$title = utf8_decode(htmlspecialchars($row->title));
+	$text  = utf8_decode(htmlspecialchars($row->text));
+	$timestamp = strtotime($row->timestamp);
+	$timestamp = date("d M Y H:i", $timestamp);
+	$grade = $row->grade;
+
+	if($grade > 0){
+		$showgrade = <<<END
+		Betyg: {$grade}
 END;
+	}
 }
+
+if(!empty($_POST))
+{
+	if(isset($_POST['publishcomment']))
+	{
+		$comment = ($_POST["comment"]);
+
+		$comment  = utf8_encode($mysqli->real_escape_string($comment));
+
+		$query = <<<END
+			INSERT INTO comment (keeperid, grid, comment, timestamp)
+			VALUES ('{$keeper}', '{$grid}', '{$comment}', CURRENT_TIMESTAMP);
+END;
+		$res = $mysqli->query($query) or die("Could not query database" . $mysqli->errno . 
+			        " : " . $mysqli->error);;
+
+	}
+}
+$query = <<<END
+	
+	SELECT * FROM comment
+	WHERE grid = '{$grid}'
+	ORDER BY timestamp DESC;
+END;
+	$res = $mysqli->query($query) or die("Could not query database" . $mysqli->errno . 
+			        " : " . $mysqli->error);
+
+	while($row = $res->fetch_object())
+	{
+		$comment = utf8_decode(htmlspecialchars($row->comment));
+		$date = strtotime($row->timestamp);
+		$date = date("d M Y H:i", $date);
+
+		$comments .=  <<<END
+		Skriven av:</br>
+		Publicerad: {$date}<br>
+		{$comment}<br><br>
+END;
+	}
+
 $content = <<<END
 
 <head>
@@ -50,6 +98,18 @@ $content = <<<END
 								Skriven av:</br>
 								Publicerad: {$timestamp}<br><br>
 								{$text}<br>	
+							</div>
+							<div class="comments">
+								<form action="genre.php?grid={$grid}" method="post">
+									<h3>Kommentarer</h3>
+									<textarea id="comment" name="comment" cols="80" rows="5"></textarea>
+									<input type="submit" id="submit" name="publishcomment" value="Kommentera">
+								</form>
+							</div>
+							<div class="showcomments">
+								<div class="panel panel-default">
+									{$comments}
+								</div>
 							</div>
 						</div>
 				</div>
