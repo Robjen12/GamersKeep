9d41b02337a628584ec14858bb6d24dface5a733
Robjen12 <Robertjensen1985@hotmail.com>
2015-04-24 10:31:04 +0200

    doo it

--- admin.php
+++ admin.php
@@ -2,7 +2,7 @@
 
 include_once("inc/HTMLTemplate.php");
 include_once("inc/Connstring.php");
-
+$grid = isset($_GET['grid']) ? $_GET['grid'] : "";
 $gr_flag = "";
 
 $query = <<<END
@@ -22,10 +22,18 @@ END;
 			$title = utf8_decode(htmlspecialchars($row->title));
 
 			$gr_flag .= <<<END
-			<a href="genre.php?grid={$grid}">{$title}</a><br>
+			<a href="genre.php?grid={$grid}">{$title}</a><button class="delete"><a href="delete.php?grid={$grid}">x</a></button>
+			<button class="admit"><a href="admin.php?grid={$grid}">V</a></button><br><br>
 END;
 		}
 	}
+
+	$admit = <<<END
+		UPDATE guidereviewinfo SET flag = 0
+		WHERE grid = '{$grid}';
+END;
+	$res = $mysqli->query($admit) or die("Could not query database" . $mysqli->errno . 
+	  " : " . $mysqli->error);
 
 	$content = <<<END
 	<div class="row margin-top-100">
@@ -37,6 +45,16 @@ END;
 
 		</div>
 	</div>
+
+	<script>
+		$(document).ready(function(){
+			$(.admit).click(function(){
+				{$admit}
+			});
+
+		})
+
+	</script>
 END;
 
 echo $header;
