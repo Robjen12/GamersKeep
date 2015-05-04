b556e5079864e4dc06d36cb49986e2aadab6fa0a
Dorte Andersen <dorteta@gmail.com>
2015-05-05 00:07:10 +0200

    header ikoner

--- inc/HTMLTemplate.php
+++ inc/HTMLTemplate.php
@@ -90,7 +90,25 @@ $(document).ready(function(){
 		})
     });
 });
+</script>
 
+<!-- header ikoner -->
+<!-- http://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_event_mouseover_mouseout -->
+<script>
+$(document).ready(function(){
+    $(".soek_button").mouseover(function(){
+        $(this).css("background-image", "url('images/search3.png')");
+	});	
+	$(".soek_button").mouseout(function(){
+        $(this).css("background-image", "url('images/search2.png')");
+    });
+	$(".header_ikoner.header_profil").mouseover(function(){
+        $(this).css("background-image", "url('images/profil3.png')");
+	});
+	$(".header_ikoner.header_profil").mouseout(function(){
+        $(this).css("background-image", "url('images/profil2.png')");
+    });
+});
 </script>
 
 	</head>
@@ -122,7 +140,7 @@ $(document).ready(function(){
 					<div class="dropdown">
 				
 					<ul class="dropdown-toggle" id="dropdownMenu1" data-toggle="dropdown" role="menu" aria-expanded="true">
-					<li class="li-icons"><a href="profile.php"><img src="images/profil2.png" class="img header-icons pull-right" title="Profil"></a>
+					<a href="profile.php" alt="Profil"><div class="header_ikoner header_profil no-border pull-right"></div></a>
 					</li>
 			  		</ul>
 					
